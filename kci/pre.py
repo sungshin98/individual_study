0220e965de75e025173ebb20bef6f4554c2361e4
sungshin98 <king202501@naver.com>
2023-07-11 19:33:36 +0900

    ADD lstm code

--- kci/pre.py
+++ kci/pre.py
@@ -1,4 +1,36 @@
-import pandas as pd
+import csv
+import os
 
-df = pd.read_csv("/EDA/Session01/Sess01_script01_User001F.csv")
-print(df.head())
+def delete_first_two_rows(csv_file):
+    temp_file = csv_file + ".tmp"  # 임시 파일 생성
+
+    with open(csv_file, 'r') as infile, open(temp_file, 'w', newline='') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+
+        # 처음 두 줄 스킵
+        next(reader)
+        next(reader)
+
+        for row in reader:
+            writer.writerow(row)
+
+    # 기존 파일 삭제 및 임시 파일 이름 변경
+    os.remove(csv_file)
+    os.rename(temp_file, csv_file)
+
+def get_file_names(folder_path):
+    file_names = []
+    for file_name in os.listdir(folder_path):
+        if os.path.isfile(os.path.join(folder_path, file_name)):
+            file_names.append(file_name)
+    return file_names
+
+folder_path = "./EDA"
+
+file_names = get_file_names(folder_path)
+print(file_names)
+script_path = os.path.abspath(__file__)
+current_directory = os.path.dirname(script_path)
+file_names = get_file_names(current_directory)
+print(file_names)
